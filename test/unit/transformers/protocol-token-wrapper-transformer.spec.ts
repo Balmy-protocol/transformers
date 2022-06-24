5b1eaf475c38516f28b258b0b13ab0964f231979
Sam Bugs <sambugs@protonmail.com>
2022-06-24 13:06:52 -0300

    feat: wrapped transform to underlying

--- test/unit/transformers/protocol-token-wrapper-transformer.spec.ts
+++ test/unit/transformers/protocol-token-wrapper-transformer.spec.ts
@@ -4,13 +4,14 @@ import { given, then, when } from '@utils/bdd';
 import { ProtocolTokenWrapperTransformer, ProtocolTokenWrapperTransformer__factory, IWETH9, ITransformer } from '@typechained';
 import { snapshot } from '@utils/evm';
 import { smock, FakeContract } from '@defi-wonderland/smock';
-import { BigNumber } from 'ethers';
+import { BigNumber, BigNumberish, utils } from 'ethers';
 
 chai.use(smock.matchers);
 
 describe('ProtocolTokenWrapperTransformer', () => {
   const PROTOCOL_TOKEN = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
   const AMOUNT_TO_MAP = 100000;
+  const RECIPIENT = '0x000000000000000000000000000000000000000F';
 
   let transformer: ProtocolTokenWrapperTransformer;
   let wToken: FakeContract<IWETH9>;
@@ -27,6 +28,7 @@ describe('ProtocolTokenWrapperTransformer', () => {
 
   beforeEach(async () => {
     await snapshot.revert(snapshotId);
+    wToken.withdraw.reset();
   });
 
   describe('constructor', () => {
@@ -73,4 +75,37 @@ describe('ProtocolTokenWrapperTransformer', () => {
       });
     });
   });
+
+  describe('transformToUnderlying', () => {
+    when('function is called', () => {
+      given(async () => {
+        // We are setting balance to the transformer, to simulate a withdraw from the wToken
+        setBalance(transformer.address, AMOUNT_TO_MAP);
+        await transformer.transformToUnderlying(wToken.address, AMOUNT_TO_MAP, RECIPIENT);
+      });
+      then('wToken is called correctly', () => {
+        expect(wToken.withdraw).to.have.been.calledOnceWith(AMOUNT_TO_MAP);
+      });
+      then('protocol token is transferred correctly to the recipient', async () => {
+        const balance = await ethers.provider.getBalance(RECIPIENT);
+        expect(balance).to.equal(AMOUNT_TO_MAP);
+      });
+      then('protocol token is removed from the transformer', async () => {
+        const balance = await ethers.provider.getBalance(transformer.address);
+        expect(balance).to.equal(0);
+      });
+      then('underlying amount is returned correctly', async () => {
+        // We are setting balance to the transformer, to simulate a withdraw from the wToken
+        setBalance(transformer.address, AMOUNT_TO_MAP);
+        const underlying = await transformer.callStatic.transformToUnderlying(wToken.address, AMOUNT_TO_MAP, RECIPIENT);
+        expect(underlying.length).to.equal(1);
+        expect(underlying[0].amount).to.equal(AMOUNT_TO_MAP);
+        expect(underlying[0].underlying).to.equal(PROTOCOL_TOKEN);
+      });
+    });
+  });
+  async function setBalance(address: string, amount: BigNumberish) {
+    const amountToSwapHex = utils.hexStripZeros(BigNumber.from(amount).toHexString());
+    await ethers.provider.send('hardhat_setBalance', [address, amountToSwapHex]);
+  }
 });

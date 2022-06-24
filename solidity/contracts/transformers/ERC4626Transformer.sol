e3d9aa4b38013f2ad918206ab1d8d67fd275c890
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2022-06-24 18:05:26 -0300

    feat: 4626 get underlying (#2)

---
+++ solidity/contracts/transformers/ERC4626Transformer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity >=0.8.7 <0.9.0;
+
+import '@openzeppelin/contracts/interfaces/IERC4626.sol';
+import '../../interfaces/ITransformer.sol';
+
+/// @title An implementaton of `ITransformer` for tokens that implement `ERC4626`
+contract ERC4626Transformer is ITransformer {
+  /// @inheritdoc ITransformer
+  function getUnderlying(address _dependent) external view returns (address[] memory) {
+    return _toUnderlying(IERC4626(_dependent).asset());
+  }
+
+  /// @inheritdoc ITransformer
+  function calculateTransformToUnderlying(address _dependent, uint256 _amountDependent) external view returns (UnderlyingAmount[] memory) {}
+
+  /// @inheritdoc ITransformer
+  function calculateTransformToDependent(address _dependent, UnderlyingAmount[] calldata _underlying)
+    external
+    view
+    returns (uint256 _amountDependent)
+  {}
+
+  /// @inheritdoc ITransformer
+  function transformToUnderlying(
+    address _dependent,
+    uint256 _amountDependent,
+    address _recipient
+  ) external returns (UnderlyingAmount[] memory) {}
+
+  /// @inheritdoc ITransformer
+  function transformToDependent(
+    address _dependent,
+    UnderlyingAmount[] calldata _underlying,
+    address _recipient
+  ) external returns (uint256 _amountDependent) {}
+
+  function _toUnderlying(address _underlying) internal pure returns (address[] memory _underlyingArray) {
+    _underlyingArray = new address[](1);
+    _underlyingArray[0] = _underlying;
+  }
+}

283d4ee5f709157ab032337cc4552a38b5733b2f
Sam Bugs <sambugs@protonmail.com>
2022-06-27 18:14:22 -0300

    feat: registry add get underlying

---
+++ solidity/contracts/test/ITransformerERC165.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.7 <0.9.0;
+
+import '@openzeppelin/contracts/utils/introspection/IERC165.sol';
+import '../../interfaces/ITransformer.sol';
+
+// Note: necessary for smocking purposes
+interface ITransformerERC165 is IERC165, ITransformer {
+
+}

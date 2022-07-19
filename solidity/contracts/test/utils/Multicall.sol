b358d3ba0f73d9abc08206a7bf8bc74274d019b2
Sam Bugs <sambugs@protonmail.com>
2022-07-19 15:54:57 -0300

    chore: fighiting slither

--- solidity/contracts/test/utils/Multicall.sol
+++ solidity/contracts/test/utils/Multicall.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.7 <0.9.0;
 import '../../utils/Multicall.sol';
 
 contract MulticallMock is Multicall {
+  // slither-disable-next-line arbitrary-send
   function sendEthToAddress(address payable _recipient, uint256 _amount) external payable {
     _recipient.transfer(_amount);
   }

bbe1d2112b2cd381186b7f2b1d499244d717d247
Sam Bugs <sambugs@protonmail.com>
2022-07-19 16:30:55 -0300

    feat: 4626 transform expected underlying

--- test/unit/transformers/erc-4626-transformer.spec.ts
+++ test/unit/transformers/erc-4626-transformer.spec.ts
@@ -170,4 +170,27 @@ describe('ERC4626Transformer', () => {
       });
     });
   });
+
+  describe('transformToExpectedUnderlying', () => {
+    when('function is called', () => {
+      given(async () => {
+        vault.withdraw.returns(AMOUNT_DEPENDENT);
+        await transformer.transformToExpectedUnderlying(
+          vault.address,
+          [{ underlying: underlyingToken.address, amount: AMOUNT_UNDERLYING }],
+          recipient.address
+        );
+      });
+      then('vault is called correctly', () => {
+        expect(vault.withdraw).to.have.been.calledOnceWith(AMOUNT_UNDERLYING, recipient.address, signer.address);
+      });
+      then('spent dependent is returned correctly', async () => {
+        const spentDependent = await transformer.callStatic.transformToExpectedUnderlying(
+          vault.address,
+          [{ underlying: underlyingToken.address, amount: AMOUNT_UNDERLYING }],
+          recipient.address
+        );
+      });
+    });
+  });
 });
